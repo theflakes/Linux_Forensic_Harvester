4e777ac2cbbf214463c5c6d080d0740a60b4e2d6
bkellogg <bk@leocybersecurity.com>
2020-04-09 21:11:01 -0400

    help update

--- src/data_def.rs
+++ src/data_def.rs
@@ -32,6 +32,8 @@ Options:
   -l, --limit           Limit CPU use
 
 Note:
+  If not run as root some telemetry cannot be harvested.
+  
   To capture network output, start a netcat listener on your port of choice.
   Use the -k option with netcat to prevent netcat from closing after a TCP connection is closed.
 
